73749ce12c2fb80c71143ebec7f2ef27f5981cd1
Daniel Pasaribu <132255073+danielpsrb@users.noreply.github.com>
2024-03-13 20:06:02 +0700

    add function insert item

--- sesi6-connectDB/services/database.py
+++ sesi6-connectDB/services/database.py
@@ -7,3 +7,8 @@ database = mysql.connector.connect(
     database='your_dbname'
 )
 
+def insert_item(kode_barang, nama_barang, stock_barang):
+    cursor = database.cursor()
+    insert = cursor.execute("INSERT INTO tbl_barang (kode_barang, nama_barang, stock_barang) VALUES (%s, %s, %s)", (kode_barang, nama_barang, stock_barang))
+    database.commit()
+    return insert
